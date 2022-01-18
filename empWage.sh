227c5cf0eb8002655874010af13a7ece872b4c1a
Studyankit <ankitghosh2018@gmail.com>
2022-01-18 09:13:53 +0530

    Calculate wage employee salary using while loop

--- empWage.sh
+++ empWage.sh
@@ -1,25 +1,37 @@
 #!/bin/bash -x
 
-isFullTime=1;
-isPartTime=2;
+perHourSalary=20;
+workingHour=0;
 totalSalary=0;
-empRatePerHrs=20;
-numWorkingDays=20;
-for ((day=1;day<=$numWorkingDays;day++))
+totalWorkingHour=0;
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
 do
-	empCheck=$((RANDOM%3));
-	case $empCheck in
-	$isFullTime)
-	 empHrs=8;
-	;;
-	$isPartTime)
-	 empHrs=4
-	;;
-	*)
-	 empHrs=0
-	;;
-esac
-	salary=$(($empRatePerHr*$empHrs));
-	totalSalary=(($totalSalary+$salary))
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
+
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	if [ $totalWorkingHour -gt 40 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
 done
-	echo $totalSalary
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
